75b7fa67c6d1777c9b00b72e166df2ac8befba14
PhiVinh <yeuthichcongviec1097@gmail.com>
2021-02-25 15:07:16 +0700

    Update enterprisewebsite.sql

--- database/enterprisewebsite.sql
+++ database/enterprisewebsite.sql
@@ -2,10 +2,10 @@
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 31, 2021 at 05:18 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.13
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th2 25, 2021 lúc 09:06 AM
+-- Phiên bản máy phục vụ: 10.4.17-MariaDB
+-- Phiên bản PHP: 7.4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `enterprisewebsite`
+-- Cơ sở dữ liệu: `enterprisewebsite`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Cấu trúc bảng cho bảng `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -34,18 +34,21 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `migrations`
+-- Đang đổ dữ liệu cho bảng `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2021_01_31_130351_create_tbl_coordinator_table', 1);
+(3, '2021_01_31_130351_create_tbl_coordinator_table', 1),
+(4, '2021_02_19_135336_tbl_course', 2),
+(5, '2021_02_20_090305_tbl_user', 3),
+(6, '2021_02_23_091011_tbl_category', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `password_resets`
+-- Cấu trúc bảng cho bảng `password_resets`
 --
 
 CREATE TABLE `password_resets` (
@@ -57,34 +60,87 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_coordinator`
+-- Cấu trúc bảng cho bảng `tbl_category`
 --
 
-CREATE TABLE `tbl_coordinator` (
-  `coordinator_id` int(10) UNSIGNED NOT NULL,
-  `coordinator_email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coordinator_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coordinator_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coordinator_phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coordinator_gender` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coordinator_address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `coordinator_birthday` date NOT NULL,
-  `coordinator_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+CREATE TABLE `tbl_category` (
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `category_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_des` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `tbl_coordinator`
+-- Đang đổ dữ liệu cho bảng `tbl_category`
 --
 
-INSERT INTO `tbl_coordinator` (`coordinator_id`, `coordinator_email`, `coordinator_password`, `coordinator_name`, `coordinator_phone`, `coordinator_gender`, `coordinator_address`, `coordinator_birthday`, `coordinator_image`, `created_at`, `updated_at`) VALUES
-(1, 'co1@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'coordinator 1', '(+084)-1234567890', 'Male', '142 Pham Phu Thu P.5 Q.6 Tp.HCM', '2000-01-06', 'coordinator1333265489754.jpg', '2021-01-31 13:24:43', '2021-01-31 13:24:43');
+INSERT INTO `tbl_category` (`category_id`, `category_name`, `category_des`, `category_status`, `created_at`, `updated_at`) VALUES
+(1, 'IT', 'Information technology (IT) is the use of computers to store, retrieve, transmit, and manipulate data[1] or information. IT is typically used within the context of business operations as opposed to personal or entertainment technologies.[2] IT is considered to be a subset of information and communications technology (ICT). An information technology system (IT system) is generally an information system, a communications system, or, more specifically speaking, a computer system – including all hardware, software, and peripheral equipment – operated by a limited group of users.', '1', NULL, NULL),
+(3, 'Design', 'A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or process. The verb to design expresses the process of developing a design. In some cases, the direct construction of an object without an explicit prior plan (such as in craftwork, some engineering, coding, and graphic design) may also be considered to be a design activity. The design usually has to satisfy certain goals and constraints, may take into account aesthetic, functional, economic, or socio-political considerations, and is expected to interact with a certain environment. Major examples of designs include architectural blueprints, engineering drawings, business processes, circuit diagrams, and sewing patterns', '1', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `tbl_course`
+--
+
+CREATE TABLE `tbl_course` (
+  `course_id` int(10) UNSIGNED NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `course_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `course_des` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `course_status` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_course`
+--
+
+INSERT INTO `tbl_course` (`course_id`, `category_id`, `course_name`, `course_des`, `course_status`, `created_at`, `updated_at`) VALUES
+(13, 3, 'Motion Graphics Designer', 'Motion graphic design is a subset of graphic design in that it uses graphic design principles in a filmmaking or video production context (or other temporally evolving visual media) through the use of animation or filmic techniques. Examples include the kinetic typography and graphics used in film and television opening sequences, and the spinning, three-dimensional station identification logos of some television channels. The art form has been around for decades, and has advanced in technical sophistication over time.', 1, NULL, NULL),
+(14, 3, 'Search engine optimization', 'Search engine optimization (SEO) is the process of improving the quality and quantity of website traffic to a website or a web page from search engines. SEO targets unpaid traffic (known as \"natural\" or \"organic\" results) rather than direct traffic or paid traffic. Unpaid traffic may originate from different kinds of searches, including image search, video search, academic search, news search, and industry-specific vertical search engines.', 1, NULL, NULL),
+(15, 1, 'Enterprise Website', 'In a nutshell, an enterprise website is a site created for an organization, business or group. The organization can be of many types and typically this type of development is done for businesses, government entities, schools, charities, and even large clubs and interest-based groups that are considered mid to large size.', 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `user_email` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_fullname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_gender` int(11) NOT NULL,
+  `user_birthday` date NOT NULL,
+  `user_role` int(11) NOT NULL,
+  `user_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `user_email`, `user_password`, `user_fullname`, `user_phone`, `user_gender`, `user_birthday`, `user_role`, `user_image`, `created_at`, `updated_at`) VALUES
+(2, 'Coordinator1@gmail.com', '123456', 'Coordinator One', '23235656598', 1, '1992-02-18', 2, 'Coordinator One1901987389UTC.png', NULL, NULL),
+(3, 'Student1@gmail.com', '123456', 'Student One1', '01213156489', 0, '2000-02-19', 1, 'Student One374064162UTC.jpg', NULL, NULL),
+(4, 'Admin1@gmail.com', '123456', 'Admin One', '03232656562', 0, '1982-02-03', 0, 'admin one786606269UTC.png', NULL, NULL),
+(5, 'Manager1@gmail.com', '123456', 'Manager One', '03213216156', 0, '1972-02-04', 3, 'Manager One1215514209UTC.jpg', NULL, NULL),
+(6, 'Guest1@gmail.com', '123456', 'Guest One', '02123154564', 1, '2001-02-04', 4, 'Guest One888711563UTC.png', NULL, NULL),
+(7, 'Student2@gmail.com', '123456', 'Student Two', '03211654489', 1, '1999-02-03', 1, 'Student Two896988113UTC.jpg', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
@@ -99,45 +155,70 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `migrations`
+-- Chỉ mục cho bảng `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_coordinator`
+-- Chỉ mục cho bảng `tbl_category`
 --
-ALTER TABLE `tbl_coordinator`
-  ADD PRIMARY KEY (`coordinator_id`);
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`category_id`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `tbl_course`
+--
+ALTER TABLE `tbl_course`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `FOREIGN` (`category_id`) USING BTREE;
+
+--
+-- Chỉ mục cho bảng `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT cho bảng `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `tbl_coordinator`
+-- AUTO_INCREMENT cho bảng `tbl_category`
 --
-ALTER TABLE `tbl_coordinator`
-  MODIFY `coordinator_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `tbl_category`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `tbl_course`
+--
+ALTER TABLE `tbl_course`
+  MODIFY `course_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
